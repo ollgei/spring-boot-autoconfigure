5dd80b208078aa98d47a88710e4119f7f986ae5f
ollgei <ollgei@sina.com>
2020-09-11 08:58:13 +0800

    feat: 代码优化

--- src/main/java/com/github/ollgei/spring/boot/autoconfigure/serialization/SerializationManager.java
+++ src/main/java/com/github/ollgei/spring/boot/autoconfigure/serialization/SerializationManager.java
@@ -1,5 +1,7 @@
 package com.github.ollgei.spring.boot.autoconfigure.serialization;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +36,7 @@ public interface SerializationManager {
         try {
             final ObjectOutput serialize = get().serialize(os);
             serialize.writeInt(object.getVersion());
-            serialize.writeObject(object.getObject());
+            serializeNativeObject(serialize, object.getObject());
             serialize.flushBuffer();
             return os.toByteArray();
         } catch (Exception ex) {
@@ -57,7 +59,7 @@ public interface SerializationManager {
             if (log.isInfoEnabled()) {
                 log.info("反序列化版本:{}", version);
             }
-            return deserialize.readObject(cls);
+            return deserializeNativeObject(deserialize, cls);
         } catch (Exception ex) {
             log.warn("反序列化失败:{}", ex.getMessage());
             throw new ErrorException(ErrorCodeEnum.FAIL_SERIALIZATION, ex);
@@ -73,28 +75,37 @@ public interface SerializationManager {
         final UnsafeByteArrayOutputStream os = new UnsafeByteArrayOutputStream();
         try {
             final ObjectOutput serialize = get().serialize(os);
-            if (object instanceof String) {
-                serialize.writeUTF((String) object);
-            } else if (object instanceof Integer) {
-                serialize.writeInt(((Integer) object).intValue());
-            } else if (object instanceof Long) {
-                serialize.writeLong(((Long) object).longValue());
-            } else if (object instanceof Double) {
-                serialize.writeDouble(((Double) object).doubleValue());
-            } else if (object instanceof Float) {
-                serialize.writeFloat(((Float) object).floatValue());
-            } else if (object instanceof Boolean) {
-                serialize.writeBool(((Boolean) object).booleanValue());
-            } else if (object instanceof Byte) {
-                serialize.writeByte(((Byte) object).byteValue());
-            } else {
-                serialize.writeObject(object);
-            }
+            serializeNativeObject(serialize, object);
             serialize.flushBuffer();
             return os.toByteArray();
         } catch (Exception ex) {
             log.warn("序列化失败:{}", ex.getMessage());
             throw new ErrorException(ErrorCodeEnum.FAIL_SERIALIZATION, ex);
+        }
+    }
+
+    /**
+     * 序列化对象.
+     * @param object object
+     * @return
+     */
+    default void serializeNativeObject(ObjectOutput serialize, Object object) throws IOException {
+        if (object instanceof String) {
+            serialize.writeUTF((String) object);
+        } else if (object instanceof Integer) {
+            serialize.writeInt(((Integer) object).intValue());
+        } else if (object instanceof Long) {
+            serialize.writeLong(((Long) object).longValue());
+        } else if (object instanceof Double) {
+            serialize.writeDouble(((Double) object).doubleValue());
+        } else if (object instanceof Float) {
+            serialize.writeFloat(((Float) object).floatValue());
+        } else if (object instanceof Boolean) {
+            serialize.writeBool(((Boolean) object).booleanValue());
+        } else if (object instanceof Byte) {
+            serialize.writeByte(((Byte) object).byteValue());
+        } else {
+            serialize.writeObject(object);
         }
     }
 
@@ -160,25 +171,35 @@ public interface SerializationManager {
         final UnsafeByteArrayInputStream is = new UnsafeByteArrayInputStream(bytes);
         try {
             final ObjectInput deserialize = get().deserialize(is);
-            if (cls == String.class) {
-                return (T) deserialize.readUTF();
-            } else if (cls == Integer.class) {
-                return (T) new Integer(deserialize.readInt());
-            } else if (cls == Long.class) {
-                return (T) new Long(deserialize.readLong());
-            } else if (cls == Double.class) {
-                return (T) new Double(deserialize.readDouble());
-            } else if (cls == Float.class) {
-                return (T) new Float(deserialize.readFloat());
-            } else if (cls == Boolean.class) {
-                return (T) new Boolean(deserialize.readBool());
-            } else if (cls == Byte.class) {
-                return (T) new Byte(deserialize.readByte());
-            }
-            return deserialize.readObject(cls);
+            return deserializeNativeObject(deserialize, cls);
         } catch (Exception ex) {
             log.warn("反序列化失败:{}", ex.getMessage());
             throw new ErrorException(ErrorCodeEnum.FAIL_SERIALIZATION, ex);
         }
+    }
+
+    /**
+     * 反序列化对象.
+     * @param deserialize deserialize
+     * @param cls class
+     * @return
+     */
+    default <T> T deserializeNativeObject(ObjectInput deserialize , Class<T> cls) throws IOException, ClassNotFoundException {
+        if (cls == String.class) {
+            return (T) deserialize.readUTF();
+        } else if (cls == Integer.class) {
+            return (T) new Integer(deserialize.readInt());
+        } else if (cls == Long.class) {
+            return (T) new Long(deserialize.readLong());
+        } else if (cls == Double.class) {
+            return (T) new Double(deserialize.readDouble());
+        } else if (cls == Float.class) {
+            return (T) new Float(deserialize.readFloat());
+        } else if (cls == Boolean.class) {
+            return (T) new Boolean(deserialize.readBool());
+        } else if (cls == Byte.class) {
+            return (T) new Byte(deserialize.readByte());
+        }
+        return deserialize.readObject(cls);
     }
 }
